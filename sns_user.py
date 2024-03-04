5ccbd263b326004cbfb740ed41e10301ec5cbd32
lovehakumai <masaotomeza@gmail.com>
2024-03-05 01:03:55 +0900

    first commit

---
+++ sns_user.py
@@ -0,0 +1,50 @@
+# ログインなどユーザに関する処理をまとめた
+from flask import Flask, session, redirect
+from functools import wraps
+
+# ユーザ名とパスワードの一覧
+USER_LOGIN_LIST = {
+    'taro':'aaa',
+    'jiro':'bbb',
+    'sabu':'ccc',
+    'siro':'ddd',
+    'goro':'eee',
+    'muro':'fff'
+}
+
+# ログインしているかの確認
+def is_login():
+    return 'login' in session
+
+# ログインを試行する
+def try_login(form):
+    user = form.get('user','')
+    password = form.get('pw','')
+    # パスワードチェック
+    if user not in USER_LOGIN_LIST:
+        return False
+    if USER_LOGIN_LIST[user] != password:
+        return False
+    session['login'] = user
+    return True
+
+# ユーザ名を得る
+def get_id():
+    return session['login'] if is_login() else '未ログイン'
+
+# 全ユーザの情報を得る
+def get_allusers():
+    return [u for u in USER_LOGIN_LIST]
+
+# ログアウトする
+def try_logout():
+    session.pop('login', None)
+
+# ログイン必須を処理するデコレーターを定義
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not is_login():
+            return redirect('/login')
+        return func(*args, **kwargs)
+    return wrapper
